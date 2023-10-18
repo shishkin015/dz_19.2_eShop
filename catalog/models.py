0c72a4b23f8116169a751a501d2e0249845ed547
shishkin015 <shishkin015@gmail.com>
2023-10-18 18:24:38 +0300

    отредактировал название в добавлении типов фикстур в админке

--- catalog/models.py
+++ catalog/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     product_description = models.TextField(verbose_name='описание продукта')
     product_preview = models.ImageField(upload_to='product/', verbose_name='превью', **NULLABLE)
     product_category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='категория')
-    product_price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='цена за покупку')
+    product_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена за покупку')
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     last_modified_date = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
 
